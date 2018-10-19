73f78f3aef70d46fc5ac7282e8bddf74b498afa6
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2018-10-19 16:35:32 +0200

    initial commit

---
+++ naples_physicians.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+#    Naples Physicians
+#
+#    ----------------------------------------------------------------------
+#    Copyright © 2018  Pellegrino Prevete
+#
+#    All rights reserved
+#    ----------------------------------------------------------------------
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+from bs4 import BeautifulSoup
+from requests import get
+import pickle
+
+def save(variable, path):
+    """Save variable on given path using Pickle
+    
+    Args:
+        variable: what to save
+        path (str): path of the output
+    """
+    with open(path, 'wb') as f:
+        pickle.dump(variable, f)
+    f.close()
+
+def load(path):
+    """Load variable from Pickle file
+    
+    Args:
+        path (str): path of the file to load
+
+    Returns:
+        variable read from path
+    """
+    with open(path, 'rb') as f:
+        variable = pickle.load(f)
+    f.close()
+    return variable
+
+class Physician:
+    """Access data from ordinemedicinapoli.it"""
+    pattern = "http://www.ordinemedicinapoli.it/scheda_medico.php?id="    
+
+    def get_from_website(self, id):
+        """Get physician data from 'ordinemedicinapoli.it' id
+
+        Args:
+            id (int): id of the physician on the website
+        
+        Returns:
+            (dict) data on the physician
+        """
+        page = get(self.pattern + str(id)).content
+        soup = BeautifulSoup(page, 'html.parser')
+        tds = soup.findAll("td")
+        persona = []
+        for el in tds:
+            if el.attrs == {}:
+               persona.append(el.string)
+        medico = {"Name":persona[0],
+                  "Surname":persona[1],
+                  "Birth date":persona[2],
+                  "Birth place":persona[3],
+                  "Albo":persona[4], 
+                  "Registration code":persona[5], 
+                  "Degree":persona[6], 
+                  "Abilitation Year":persona[7]}
+        return medico
+
+    def save_all_to_disk(self):
+        """Save on disk all physicians from 'ordinemedicinapoli.it'"""
+        physicians = []
+        for i in range(1,25149):
+            if i % 1000 == 0:
+                print(i)
+            physicians.append(get_data(i))
+
+        save(physicians, 'naples_physicians.pkl')
+
+Physician().save_all_to_disk()
---
+++ wikidata_physicians.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+#    Wikidata Physicians
+#
+#    ----------------------------------------------------------------------
+#    Copyright © 2018  Pellegrino Prevete
+#
+#    All rights reserved
+#    ----------------------------------------------------------------------
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+import pickle
+from pprint import pprint
+from pywikibot import ItemPage, Site
+from pywikibot.data.sparql import SparqlQuery
+from re import sub
+
+#site = Site('wikidata', 'wikidata')
+#repo = site.data_repository()
+
+def save(variable, path):
+    """Save variable on given path using Pickle
+    
+    Args:
+        variable: what to save
+        path (str): path of the output
+    """
+    with open(path, 'wb') as f:
+        pickle.dump(variable, f)
+    f.close()
+
+
+class WikidataPhysicians:
+    def get_all(self):
+        physician = 'Q39631'
+        
+        query = """SELECT ?subclass_of WHERE {
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+          ?subclass_of wdt:P279 wd:Q39631.
+        }
+        """
+        sparql = SparqlQuery()
+        results = sparql.query(query)
+        
+        physician_types = set([q['subclass_of']['value'].split("/")[-1] for q in results['results']['bindings']])
+        physician_types.add(physician)
+        
+        physicians = set()
+        
+        for t in physician_types:
+            query = sub('physician_type', t, """SELECT ?physician WHERE {
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+          ?physician wdt:P106 wd:physician_type.
+          ?physician wdt:P31 wd:Q5.
+        }
+        """)
+            sparql = SparqlQuery()
+            try:
+                results = sparql.query(query)
+                results = set([q['physician']['value'].split("/")[-1] for q in results['results']['bindings']])
+            except Exception as e:
+                pprint(e)
+            physicians = physicians.union(results)
+        return physicians
+
+    def save_all_to_disk(self):
+        physicians = self.get_all()
+        save(physicians, 'wikidata_physicians.pkl')
+
+physician = WikidataPhysician()
+physician.save_all_to_disk()
+
